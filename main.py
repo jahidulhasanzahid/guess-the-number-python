636f22872f8694f6664dd1a1f2bba913d60ecd49
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-01-29 10:07:46 -0500

    guess number.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello Guess the game")
+import  random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    count_guess = 0
+    while count_guess != random_number:
+        count_guess = int(input(f'Guess number between 1 to {x}: '))
+        if count_guess < random_number:
+            print('Sorry! Guess Again. Too Low')
+            print(count_guess)
+            print(random_number)
+        elif count_guess > random_number:
+            print('Sorry! Guess Again. Too High')
+
+    print(f'Wow, You are Correct. You guess the number {random_number}')
+
+guess(10)
